c1d1de425e6f73423c49d8b89a4f6a97fb02361a
Justin <me@justinle.page>
2021-06-28 14:06:44 -0700

    Updates

---
+++ precache-manifest.e0a8b40c0481cd51b2f8359d31813996.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06beed5613f37b521ead6ef7aaaffd32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5e96c81f1cca3923e13",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c299da29e84d6c646471",
+    "url": "/static/css/main.f18904f5.chunk.css"
+  },
+  {
+    "revision": "f5e96c81f1cca3923e13",
+    "url": "/static/js/2.b7eeeb13.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b7eeeb13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c299da29e84d6c646471",
+    "url": "/static/js/main.5a41ce65.chunk.js"
+  },
+  {
+    "revision": "314fa0fef1e4488bd2c5",
+    "url": "/static/js/runtime-main.bbc180e6.js"
+  }
+]);
